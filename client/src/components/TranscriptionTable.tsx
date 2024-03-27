6dfff0bdd0c156cfdb5f79d90aada50e86eb7832
Marco <markcostah@gmail.com>
2024-03-27 14:48:20 +0000

    Simplified the project

--- client/src/components/TranscriptionTable.tsx
+++ client/src/components/TranscriptionTable.tsx
@@ -1,13 +1,18 @@
 import '../index.css'
+import React, { useState } from 'react';
+import { processTranscription } from '../utils/api-client';
+import { error } from 'console';
 
 type TableProps = {
   file?:File
 }
 
 export const TranscriptionTable:React.FC<TableProps> = ({file}):JSX.Element => {
+  
+
   return (
     <div className='px-7'>
-      <div className="relative overflow-x-auto mt-5">
+      {/* <div className="relative overflow-x-auto mt-5">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border border-gray-700">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 bg-slate-800 text-gray-400 border border-gray-700">
           <tr>
@@ -33,7 +38,14 @@ export const TranscriptionTable:React.FC<TableProps> = ({file}):JSX.Element => {
                   {file.name}
                 </th>
                 <td className="px-6 py-4">{file.size} MB</td>
-                <td className="px-6 py-4">Processing</td>
+
+                {startedTranscription ? 
+                  <td className="px-6 py-4">Processing</td> : 
+                  <div className="px-6 py-4">
+                    <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow text-xs h-8 w-20" onClick={handleTranscription}>Start</button>
+                  </div> 
+                  }
+
                 <td className="">Send</td>
               </>
             :
@@ -42,7 +54,7 @@ export const TranscriptionTable:React.FC<TableProps> = ({file}):JSX.Element => {
           </tr>
         </tbody>
       </table>
-    </div>
+    </div> */}
     </div>
   );
 }
