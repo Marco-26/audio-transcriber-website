4b839c05a879af5e95aa7d15fb6a6e94109d77c1
Marco-26 <markcostah@gmail.com>
2024-05-14 13:06:53 +0100

    Refactor: Changes in model

--- server/models.py
+++ server/models.py
@@ -7,7 +7,6 @@ class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.Text, nullable=False)
   email = db.Column(db.Text, nullable=False, unique=True)
-  password = db.Column(db.Text, nullable=False)
 
   def __repr__(self):
     return f'<User:{self.name}>'
