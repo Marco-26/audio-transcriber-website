9793f95806143a58af2497f7f5fdb957da5e2cee
Marco-26 <markcostah@gmail.com>
2024-08-04 12:44:03 +0100

    Feature: Add lifetime to session

--- server/config.py
+++ server/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class ApplicationConfig:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
@@ -8,7 +9,8 @@ class ApplicationConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 
     SESSION_TYPE = 'sqlalchemy'
-    SESSION_PERMANENT = False
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
     SESSION_USE_SIGNER = True
     
     SESSION_COOKIE_SECURE = True
