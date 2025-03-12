71f76e152b6e0afa6f44b63480385b4ee2ca9212
Marco-26 <markcostah@gmail.com>
2025-03-12 19:30:05 +0000

    Feat: Delete file in S3

--- server/app/services/transcription_service.py
+++ server/app/services/transcription_service.py
@@ -27,53 +27,59 @@ def create_file_entry(user_id, filename, unique_filename, file_info, file_path):
   db.session.commit()
   return file_entry
 
-def delete_file(file):
+def delete_file(file:FileEntry):
+  if file.transcribed:
+    transcribed_filename = file.unique_filename+"-transcribed.txt"
+    s3_service.delete_file(transcribed_filename)
+  else:
+    s3_service.delete_file(file.unique_filename)
+      
   db.session.delete(file)
   db.session.commit()
 
 async def transcribe_and_save(file_path:str, file_entry:FileEntry):
   if not os.path.exists(file_path):
-      raise FileNotFoundError("Audio file not found")
+    raise FileNotFoundError("Audio file not found")
 
   try:
-      transcript = await asyncio.to_thread(transcribe_audio, file_path)
+    transcript = await asyncio.to_thread(transcribe_audio, file_path)
 
-      if not transcript:
-          raise ValueError("Transcription failed, no transcript generated.")
-      
-      transcription_file_name = file_entry.unique_filename+"-transcribed.txt"
-      transcript_file_path = file_path + transcription_file_name
-      with open(transcript_file_path, 'w') as temp_file:
-          temp_file.write(transcript)
-          temp_file.flush()
-      
-      s3_service.upload(transcript_file_path,transcription_file_name)
-      s3_service.delete_file(file_entry.unique_filename)
-      os.remove(file_path)
-      os.remove(transcript_file_path)
-      
-      file_entry.transcribed = True
-      db.session.add(file_entry)
-      db.session.commit()
+    if not transcript:
+        raise ValueError("Transcription failed, no transcript generated.")
+    
+    transcription_file_name = file_entry.unique_filename+"-transcribed.txt"
+    transcript_file_path = file_path + transcription_file_name
+    with open(transcript_file_path, 'w') as temp_file:
+        temp_file.write(transcript)
+        temp_file.flush()
+    
+    s3_service.upload(transcript_file_path,transcription_file_name)
+    s3_service.delete_file(file_entry.unique_filename)
+    os.remove(file_path)
+    os.remove(transcript_file_path)
+    
+    file_entry.transcribed = True
+    db.session.add(file_entry)
+    db.session.commit()
 
   except Exception as e:
-      print(f"Error during transcription: {str(e)}")
-      raise e
+    print(f"Error during transcription: {str(e)}")
+    raise e
 
 def validate_user_and_file(user_id, file_id):
-    user = user_service.get_user_by_id(user_id)
-    if not user:
-        return jsonify(error="User not found"), 404, None, None, None
+  user = user_service.get_user_by_id(user_id)
+  if not user:
+      return jsonify(error="User not found"), 404, None, None, None
 
-    file_entry = get_file_by_id(file_id)
-    if not file_entry:
-        return jsonify(error="File entry not found in the database"), 404, None, None, None
+  file_entry = get_file_by_id(file_id)
+  if not file_entry:
+      return jsonify(error="File entry not found in the database"), 404, None, None, None
 
-    if file_entry.user_id != user.id:
-        return jsonify(error="The file doesn't belong to this user"), 403, None, None, None
+  if file_entry.user_id != user.id:
+      return jsonify(error="The file doesn't belong to this user"), 403, None, None, None
 
-    file_path = s3_service.download(file_entry.unique_filename)
-    print(file_path)
-    if not file_path:
-       return jsonify(error="File not found"), 404, None, None, None
-    return None, None, user, file_path, file_entry
+  file_path = s3_service.download(file_entry.unique_filename)
+  print(file_path)
+  if not file_path:
+      return jsonify(error="File not found"), 404, None, None, None
+  return None, None, user, file_path, file_entry
