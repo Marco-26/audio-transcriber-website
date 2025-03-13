ae53dd3cbbbd00fe6902071590c0ef1cc64e8c1f
Marco-26 <markcostah@gmail.com>
2025-03-13 23:16:48 +0000

    Refactor: Custom exceptions in auth controller

--- server/app/controllers/auth_controller.py
+++ server/app/controllers/auth_controller.py
@@ -3,34 +3,36 @@ from flask import Blueprint, jsonify, request, session
 from ..app import allowed_users, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 from ..models.user import User
 from ..services import auth_service, user_service
+from ..exceptions.api_error import APIBadRequestError, APIAuthError
+
 auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/auth/login', methods=['POST'])
 def login():
-    auth_code = request.get_json()['code']
+  auth_code = request.get_json().get('code')
 
-    if not auth_code:
-        return jsonify(success=False, error="An error ocurred, please try again later."), 400
-    
-    token = auth_service.auth_code_for_token(auth_code)
-    if not token:
-        return jsonify(success=False, error="An error ocurred, please try again later."), 401
-    
-    user_info = auth_service.get_user_info_by_token(token)
-    if user_info["email"] not in allowed_users:
-        return jsonify(success=False, error="Access denied. This application is currently in a restricted testing phase."), 403 
+  if not auth_code:
+    raise APIBadRequestError("Missing authentication code.")
 
-    user = user_service.get_user_by_email(user_info["email"])
-    
-    if not user:
-        user = user_service.create_user(user_info=user_info)
+  token = auth_service.auth_code_for_token(auth_code)
+  if not token:
+    raise APIAuthError("Authentication failed. Invalid authorization code.")
 
-    session["user_id"] = user.id
-    print("User id: " + str(user.id))
-    return jsonify(success=True, message="User logged in successfully.", payload=user.to_dict())
+  user_info = auth_service.get_user_info_by_token(token)
+  if user_info["email"] not in allowed_users:
+    raise APIAuthError("Access denied. This application is currently in a restricted testing phase.")
 
+  user = user_service.get_user_by_email(user_info["email"])
+  if not user:
+    user = user_service.create_user(user_info=user_info)
+
+  session["user_id"] = user.id
+  return jsonify(success=True, message="User logged in successfully.", payload=user.to_dict())
 
 @auth_bp.route("/auth/logout", methods=["POST"])
 def logout_user():
-    session.pop("user_id")
-    return "200"
+  if "user_id" not in session:
+    raise APIAuthError("Unauthorized logout attempt. No active session found.")
+
+  session.pop("user_id")
+  return jsonify(success=True, message="User logged out successfully."), 200
